f139f9bd6e50ad70feddd8e2bf8b422225fef321
Pitchapa Pawong <>
2024-05-24 16:04:04 +0700

    [✨] hero section

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import avatarBg from "@/public/avatar-bg.svg";
+import avatar from "@/public/avatar.png"
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="bg-gradient-section min-h-[80dvh]">
+        <div className="px-40 pt-40 flex items-center">
+          <div className="max-w-[36dvw] h-full">
+            <h2 className="text-primary">Hi, my name is</h2>
+            <h1 className="text-primary">Pitchapa Pawong.</h1>
+            <p className="text-body-1 text-white mt-4">
+              A front-end developer with over 4 years of professional
+              experience, specializing in React to deliver excellent user
+              experiences.
+            </p>
+          </div>
+          <div className="relative">
+            <Image
+              src={avatarBg}
+              width={576}
+              height={446}
+              alt="avatar-bg-svg"
+            />
+            <Image
+              src={avatar}
+              width={446}
+              height={446}
+              alt="avatar-ploy"
+              style={{ position: "absolute", top: 0, marginTop: "-37px", marginLeft: "104px"}}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
