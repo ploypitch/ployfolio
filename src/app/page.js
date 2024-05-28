ce4acc6cf8303177ac7ef6402c5c49c344ca7b7e
Pitchapa Pawong <>
2024-05-28 21:09:05 +0700

    [➕] add section to page

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,17 @@ import Image from "next/image";
 import HeroSection from "./sections/HeroSection";
 import MySkill from "./sections/MySkill";
 import Experiences from "./sections/Experiences";
+import SeeMore from "./sections/SeeMore";
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
       <HeroSection />
       <MySkill />
       <Experiences />
+      <SeeMore />
     </main>
   );
 }
