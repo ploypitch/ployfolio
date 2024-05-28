4d6125ecd284e5bf554904d5bc27052887d2038e
Pitchapa Pawong <>
2024-05-28 21:17:12 +0700

    [✨] Footer

---
+++ src/app/sections/Footer.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark-400 p-16 h-dvh">
+        footer
+    </div>
+  );
+};
+
+export default Footer;
