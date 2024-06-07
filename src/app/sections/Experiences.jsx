27785055bb8c525ce3cb6d077a65b24a2072621d
Pitchapa Pawong <>
2024-06-07 17:17:56 +0700

    [💄] update typo tag

--- src/app/sections/Experiences.jsx
+++ src/app/sections/Experiences.jsx
@@ -8,7 +8,7 @@ const Experiences = () => {
       <div className="bg-secondary h-dvh">
         <div className="px-40 flex items-center justify-between h-full">
           <div className="w-6/12">
-            <h1 className="text-primary">Over the years,</h1>
+            <p className="text-h1 text-primary">Over the years,</p>
             <p className="text-body-1 text-white mt-6">
               I&lsquo;ve built products for companies and businesses around the globe
               ranging from marketing websites to complex solutions and
