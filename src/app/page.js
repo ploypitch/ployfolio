0e5d61358d1444e200e2726dd88f3af39280fbce
Pitchapa Pawong <>
2024-06-10 00:29:52 +0700

    [➕] add element for section

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,35 @@
-import Image from "next/image";
+"use client";
 
+import React from "react";
 import HeroSection from "./sections/HeroSection";
 import MySkill from "./sections/MySkill";
 import Experiences from "./sections/Experiences";
 import SeeMore from "./sections/SeeMore";
-import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 import Footer from "./sections/Footer";
+
+import { Element } from "react-scroll";
 
 export default function Home() {
   return (
     <>
-      <NavBar />
+      <Header />
       <main>
-        <HeroSection />
-        <MySkill />
-        <Experiences />
-        <SeeMore />
-        <Footer />
+        <Element name="hero-section" id="hero">
+          <HeroSection />
+        </Element>
+        <Element name="my-skill" id="skill">
+          <MySkill />
+        </Element>
+        <Element name="experience" id="exp">
+          <Experiences />
+        </Element>
+        <Element name="see-more" id="more">
+          <SeeMore />
+        </Element>
+        <Element name="footer" id="foot">
+          <Footer />
+        </Element>
       </main>
     </>
   );
