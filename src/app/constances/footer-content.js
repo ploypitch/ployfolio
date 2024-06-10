14ee4c35788518f8923a80200cc6a55116f869f5
Pitchapa Pawong <>
2024-06-10 19:52:37 +0700

    [✨] constance - footer

---
+++ src/app/constances/footer-content.js
@@ -0,0 +1,49 @@
+export const FooterContent = [
+  {
+    title: "About me",
+    children: [
+      {
+        title: "Homepage",
+        href: "/",
+      },
+      {
+        title: "My resume",
+        href: "/",
+      },
+    ],
+  },
+  {
+    title: "My work",
+    children: [
+      {
+        title: "Vonder Workplace",
+        href: "/",
+      },
+      {
+        title: "Vonder project",
+        href: "/",
+      },
+      {
+        title: "VerMe",
+        href: "/",
+      },
+    ],
+  },
+  {
+    title: "About me",
+    children: [
+      {
+        title: "Pitchapa.paw@gmail.com",
+        href: "/",
+      },
+      {
+        title: "Linkedin",
+        href: "/",
+      },
+      {
+        title: "Github",
+        href: "/",
+      },
+    ],
+  },
+];
