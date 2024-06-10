217ae32151f931ea576272a82d5702909c22f860
Pitchapa Pawong <>
2024-06-10 19:52:54 +0700

    [✨] constance - my work

---
+++ src/app/constances/my-work.js
@@ -0,0 +1,19 @@
+import Tonton from "@/public/tonton.jpg";
+
+export const MyWorkList = [
+    {
+        imageSrc: Tonton,
+        label: "Vonder Workplace",
+        desc: "React.js, Antd, Cypress"
+    },
+    {
+        imageSrc: Tonton,
+        label: "Vonder Project",
+        desc: "React.js, Antd"
+    },
+    {
+        imageSrc: Tonton,
+        label: "VerME",
+        desc: "Next.js, tailwind, mui"
+    }
+]
