a2db6b5f3227ce79273959c63cf963084b56ab52
Pitchapa Pawong <>
2024-06-10 19:37:44 +0700

    [👌] edit value

--- src/app/sections/my-skill.jsx
+++ src/app/sections/my-skill.jsx
@@ -4,22 +4,11 @@ import React from "react";
 import Image from "next/image";
 import styled from "styled-components";
 
-import antdIcon from "@/public/skills/antd.png";
-import cssIcon from "@/public/skills/css.png";
-import emotionIcon from "@/public/skills/emotion.png";
-import figmaIcon from "@/public/skills/figma.png";
-import gitIcon from "@/public/skills/git.png";
-import htmlIcon from "@/public/skills/html.png";
-import javascriptIcon from "@/public/skills/javascript.png";
-import nextIcon from "@/public/skills/next.png";
-import reactIcon from "@/public/skills/react.png";
-import styledComponentIcon from "@/public/skills/styled-component.png";
-import tailwindIcon from "@/public/skills/tailwind.png";
-import typescriptIcon from "@/public/skills/typescript.png";
-
 import bgVector1 from "@/public/bg_vector_1.svg?url";
 import bgVector2 from "@/public/bg_vector_2.svg?url";
 import bgVector3 from "@/public/bg_vector_3.svg?url";
+
+import { skillsArr } from "../constances/my-skill";
 
 const Divider = styled.div`
   width: 2px;
@@ -28,27 +17,6 @@ const Divider = styled.div`
 `;
 
 const MySkill = () => {
-  const skillsArr = [
-    {
-      title: "Languages",
-      iconSrc: [javascriptIcon, typescriptIcon, htmlIcon, cssIcon],
-    },
-    {
-      title: "Libraries and Frameworks",
-      iconSrc: [
-        reactIcon,
-        nextIcon,
-        antdIcon,
-        tailwindIcon,
-        styledComponentIcon,
-        emotionIcon,
-      ],
-    },
-    {
-      title: "Tools",
-      iconSrc: [figmaIcon, gitIcon],
-    },
-  ];
 
   return (
     <section>
