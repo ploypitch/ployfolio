8e648b2a4274ffb37b0fa006d13346f6850f28af
Pitchapa Pawong <>
2024-06-10 19:50:38 +0700

    [➕] add check isLandingPage

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,16 +2,23 @@
 
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import logo from "@/public/logo.svg?url";
-import { Link } from "react-scroll";
+import { Link as Scroll } from "react-scroll";
 
-const Header = () => {
+const Header = ({ isLandingPage }) => {
   return (
     <div className="w-full xs:px-8 lg:px-20 py-6 flex items-center lg:fixed xs:absolute z-10">
-      <Link to="hero-section" spy={true} smooth={true} duration={500}>
-        <Image src={logo} alt="p-logo" width={32} />
-      </Link>
+      {isLandingPage ? (
+        <Scroll to="hero-section" spy={true} smooth={true} duration={500}>
+          <Image src={logo} alt="p-logo" width={32} />
+        </Scroll>
+      ) : (
+        <Link href="/">
+          <Image src={logo} alt="p-logo" width={32} />
+        </Link>
+      )}
     </div>
   );
 };
