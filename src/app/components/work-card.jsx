8924cfcebaf21b0276df9789daf705df7a674ae0
Pitchapa Pawong <>
2024-06-10 19:51:24 +0700

    [✨] work card component

---
+++ src/app/components/work-card.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+import Tonton from "@/public/tonton.jpg";
+
+const WorkCard = ({
+  imageSrc,
+  label = "Label",
+  desc = "React.js, Antd, Cypress",
+}) => {
+  return (
+    <div className="w-full xs:h-32 md:h-[164px] xs:p-3 md:p-4 bg-card-bg border rounded-2xl border-card-border flex gap-4">
+      <div className="xs:w-[104px] md:w-[250px]">
+        <Image
+          src={Tonton}
+          alt="cover"
+          width={"auto"}
+          height={"auto"}
+          className="rounded-2xl object-cover xs:h-[104px] md:h-[132px]"
+        />
+      </div>
+      <div className="flex flex-col justify-center h-full">
+        <p className="xs:text-h3-mb md:text-h3 text-dark">{label}</p>
+        <p className="xs:text-body-2-mb md:text-body-2 text-dark-gray">{desc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default WorkCard;
