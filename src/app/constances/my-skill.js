2e1cfcb78a4ffdf9bd6d265d9baaf34a73dab45b
Pitchapa Pawong <>
2024-06-10 19:51:55 +0700

    [✨] constance - my skill

---
+++ src/app/constances/my-skill.js
@@ -0,0 +1,34 @@
+import antdIcon from "@/public/skills/antd.png";
+import cssIcon from "@/public/skills/css.png";
+import emotionIcon from "@/public/skills/emotion.png";
+import figmaIcon from "@/public/skills/figma.png";
+import gitIcon from "@/public/skills/git.png";
+import htmlIcon from "@/public/skills/html.png";
+import javascriptIcon from "@/public/skills/javascript.png";
+import nextIcon from "@/public/skills/next.png";
+import reactIcon from "@/public/skills/react.png";
+import styledComponentIcon from "@/public/skills/styled-component.png";
+import tailwindIcon from "@/public/skills/tailwind.png";
+import typescriptIcon from "@/public/skills/typescript.png";
+
+export const skillsArr = [
+  {
+    title: "Languages",
+    iconSrc: [javascriptIcon, typescriptIcon, htmlIcon, cssIcon],
+  },
+  {
+    title: "Libraries and Frameworks",
+    iconSrc: [
+      reactIcon,
+      nextIcon,
+      antdIcon,
+      tailwindIcon,
+      styledComponentIcon,
+      emotionIcon,
+    ],
+  },
+  {
+    title: "Tools",
+    iconSrc: [figmaIcon, gitIcon],
+  },
+];
