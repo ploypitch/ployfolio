a5879bb1d9eca76dee58793605b2eb7ca60d8d43
Pitchapa Pawong <>
2024-06-12 02:38:18 +0700

    [✨] work project pages

---
+++ src/app/my-work/vonder-workplace/page.jsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+
+import Layout from "@/components/layout";
+
+const VonderWorkplace = () => {
+  return (
+    <Layout isLandingPage={false}>
+      <div className="bg-gray h-full overflow-hidden flex justify-center w-full">
+        <div className="xs:px-6 xl:px-40 py-28 flex flex-col xs:w-full xl:w-[1200px]">
+          <p className="text-h1 text-dark mb-16">Vonder Workplace</p>
+          Coming soon...
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default VonderWorkplace;
