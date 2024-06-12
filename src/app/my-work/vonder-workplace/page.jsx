f932ceebce56d16a5edeba79d4933b8c5f50c498
Pitchapa Pawong <>
2024-06-12 22:40:22 +0700

    [➕] add back button

--- src/app/my-work/vonder-workplace/page.jsx
+++ src/app/my-work/vonder-workplace/page.jsx
@@ -2,11 +2,13 @@
 import React from "react";
 
 import Layout from "@/components/layout";
+import BackButton from "@/app/components/back-button";
 
 const VonderWorkplace = () => {
   return (
     <Layout isLandingPage={false}>
       <div className="bg-gray h-full overflow-hidden flex justify-center w-full">
+        <BackButton />
         <div className="xs:px-6 xl:px-40 py-28 flex flex-col xs:w-full xl:w-[1200px]">
           <p className="text-h1 text-dark mb-16">Vonder Workplace</p>
           Coming soon...
