658485f91d3fbbd2828605a4c58573b0d728f7a0
Pitchapa Pawong <>
2024-06-13 01:56:14 +0700

    [➕] image array for vonder wf

---
+++ src/app/components/mywork-layout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Layout from "./layout";
+import BackButton from "./back-button";
+
+const MyWorkLayout = ({ project = "Vonder Workplace", children }) => {
+  return (
+    <>
+      <Layout isLandingPage={false}>
+        <div className="bg-gray h-full overflow-hidden flex justify-center w-full">
+          <BackButton />
+          <div className="xs:px-6 xl:px-40 py-28 flex flex-col xs:w-full items-center">
+            <p className="text-h1 text-dark mb-16">{project}</p>
+            {children}
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default MyWorkLayout;
